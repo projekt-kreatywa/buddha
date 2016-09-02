37df970ca9f7be22fe886a18c7293872c35d8d2d
@rafszul <rafszul@gmail.com>
2016-09-02 17:10:12 +0100

    slight change

--- build/script.js
+++ build/script.js
@@ -33,10 +33,17 @@
 				scene.add( directionalLight );
 
 				// BEGIN Clara.io JSON loader code
-				var objectLoader = new THREE.ObjectLoader();
-				objectLoader.load("model/untitled-scene.json", function ( obj ) {
-				 	scene.add( obj );
-				} );
+
+				var loader = new THREE.ObjectLoader();
+loader.load("model/untitled-scene.json",function ( obj ) {
+     scene.add( obj );
+});
+
+				//var objectLoader = new THREE.ObjectLoader();
+				//objectLoader.load("model/untitled-scene.json", function ( obj ) {
+				// 	scene.add( obj );
+				//} );
+				
 				// END Clara.io JSON loader code
 
 				renderer = new THREE.WebGLRenderer();
